9f28b56a5cdd4ff5be6fde52b0def2eae0813eb0
alwaki <xalwa999@gmail.com>
2023-04-05 18:53:53 +0200

    removed deprecated F23 function

--- src/winograd.jl
+++ src/winograd.jl
@@ -1,20 +1,4 @@
 """
-    F23(d, g, b2, b3)
-
-    # Arguments
-    - `d::Array{Float}`: Array
-    - `g::Array{Float}`: Array 
-    - `b2::Float`: Multiplication constant
-    - `b3::Float`: Multiplication constant 
-    
-Computes the Winograd convolution with output size 2 and filter size 3
-"""
-function F23(d, g, b2, b3)
-    return [((d[1] - d[3]) * g[1])+((d[2] + d[3]) * b2)+((d[3] - d[2]) * b3)
-     ((d[2] + d[3]) * b2)-((d[3] - d[2]) * b3)-((d[2] - d[4]) * g[3])]
-end
-
-"""
     Winograd1D(data, filter, N, output_list, b2, b3, i)
 
     # Arguments
