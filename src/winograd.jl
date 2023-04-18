025bd1d968fc63cc588b129f412f0e471cc3937d
alwaki <xalwa999@gmail.com>
2023-04-18 21:35:00 +0200

    Changed allocation, no allocations occur now

--- src/winograd.jl
+++ src/winograd.jl
@@ -7,7 +7,6 @@
     - `N::Integer`: Length of data
     - `output_list::Array{Float}`: Array to output convolved data into 
     - `b::Array{Float}`: Multiplication constants
-    - `i::Integer`: Iteration variable
 
     Computes the Winograd convolution with output size 2 and filter size 3. Note that 
 this function works only on 1D data and filters.
@@ -16,8 +15,8 @@ function Winograd1D!(d, g, N, s, b)
     @inline for i = 1:2:N-1
         a1 = (d[2] + d[3]) * b[1]
         a2 = (d[3] - d[2]) * b[2]
-        s[i:i+1] = [((d[1] - d[3]) * g[1])+a1+a2
-        a1-a2-((d[2] - d[4]) * g[3])]
+        s[i] = ((d[1] - d[3]) * g[1])+a1+a2
+        s[i+1] = a1-a2-((d[2] - d[4]) * g[3])
     end
     return s
 end
