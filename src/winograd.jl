46ed8f14fee71d9e4e2ba162e074493031330b1f
alwaki <xalwa999@gmail.com>
2023-04-25 23:33:16 +0200

    optimizing 2D matrix winograd, overhauled docs

--- src/winograd.jl
+++ src/winograd.jl
@@ -1,32 +1,47 @@
 """
-    Winograd1D(data, filter, N, output_list, b2, b3, i)
+    Winograd1D!(d, f, N, output_list, b2, b3)
 
     # Arguments
-    - `data::Array{}`: Data array to convolve
-    - `filter::Array{}`: Filter array to convolve with 
+    - `d::Array{}`: Data array to convolve
+    - `f::Array{}`: Filter array to convolve with 
     - `N::Integer`: Length of data
-    - `output_list::Array{}`: Array to output convolved data into 
-    - `b::Array{Float}`: Multiplication constants
+    - `out::Array{}`: Array to output convolved data into 
+    - `b::Array{}`: Multiplication constants
 
-    Computes the Winograd convolution with output size 2 and filter size 3. Note that 
+Computes the Winograd convolution with output size 2 and filter size 3. Note that 
 this function works only on 1D data and filters.
 """
-function Winograd1D!(d, g, N, s, b)
+function Winograd1D!(d::AbstractVector, f::AbstractVector, N::Int,
+     out::AbstractVector, b::AbstractVector)
     @inline for i = 1:2:N-1
         a1 = (d[2] + d[3]) * b[1]
         a2 = (d[3] - d[2]) * b[2]
-        s[i] = ((d[1] - d[3]) * g[1])+a1+a2
-        s[i+1] = a1-a2-((d[2] - d[4]) * g[3])
+        out[i] = ((d[1] - d[3]) * f[1])+a1+a2
+        out[i+1] = a1-a2-((d[2] - d[4]) * f[3])
     end
-    return s
+    return out
 end
 
-function WinogradMatrix2D(filter, data, G, A, B, Gt, At, Bt)
-    filter2d = [1.0 1.0 1.0; 1.0 2.0 1.0; 1.0 1.0 1.0]
-    data2d = [0.0 1.0 1.0 1.0; 2.0 3.0 4.0 5.0; 1.0 2.0 3.0 4.0; 0.0 2.0 1.0 3.0]
-    At = [1 1 1 0; 0 1 -1 -1]
-    G = [1 0 0; 0.5 0.5 0.5; 0.5 -0.5 0.5; 0 0 1]
-    Bt = [1 0 -1 0; 0 1 1 0; 0 -1 1 0; 0 1 0 -1]
+"""
+    WinogradMatrix2D!(d, out, dw, dh, AtGFGtBt, BA)
 
-    return At*((G*filter2d*transpose(G)).*(Bt*data2d*transpose(Bt)))*transpose(At)
+    # Arguments
+    - `d::Matrix{}`: Data matrix to convolve
+    - `out::Matrix{}`: Array to output convolved data into 
+    - `dw::Integer`: Width of data
+    - `dh::Integer`: Height of data
+    - `AtGFGtBt::Matrix{}`: Multiplication constants matrix, includes filter
+    - `BA::Matrix{}`: Multiplication constants matrix
+
+Computes the Winograd convolution with output size 2x2 and filter size 3x3. Note that 
+this function utilizes matrix multiplication.
+"""
+function WinogradMatrix2D!(d::AbstractMatrix, out::AbstractMatrix, 
+    dw::Int, dh::Int, AtGFGtBt::AbstractMatrix, BA::AbstractMatrix)
+    @inline for i = 1:2:dh
+        @inline for j = 1:2:dw
+            out[i:i+1,j:j+1] = AtGFGtBt*d[i:i+3,j:j+3]*BA
+        end
+    end
+    return out
 end
