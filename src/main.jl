4aafaabf83f40f0b63cd0139765aa7d329561564
alwaki <xalwa999@gmail.com>
2023-04-26 17:05:14 +0200

    added single benchmark for testing

--- src/main.jl
+++ src/main.jl
@@ -31,5 +31,28 @@ filter = [1.0, 2.0, 1.0]
 #test1D(filter, datapoints);
 
 # Run 2D test
-datapoints2d = exp10.(range(1, stop=3, length=10))
-test2D(filter, datapoints2d)
+datapoints2d = exp10.(range(1, stop=3, length=4))
+#test2D(filter, datapoints2d)
+
+# Auxiliary testing
+
+filter2d = filter*transpose(filter);
+# Winograd precomputation
+At = [1 1 1 0; 0 1 -1 -1]
+A = transpose(At)
+G = [1 0 0; 0.5 0.5 0.5; 0.5 -0.5 0.5; 0 0 1]
+Gt = transpose(G)
+Bt = [1 0 -1 0; 0 1 1 0; 0 -1 1 0; 0 1 0 -1]
+B = transpose(Bt)
+AtGFGtBt = At*((G*filter2d*Gt).*Bt)
+BA = B * A
+data2d = rand(Float64, (Int64(floor(10)), Int64(floor(10))))
+data2d_padded = zeropad2D(filter2d, data2d);
+dw = size(data2d_padded,2)
+dh = size(data2d_padded,1)
+dw1 = dw-size(filter2d,2)
+dh1 = dh-size(filter2d,1)
+output_array = zeros(size(data2d));
+
+@benchmark WinogradMatrix2D!($data2d_padded, $output_array,
+$dw1, $dh1, $AtGFGtBt, $BA)
